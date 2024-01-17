408eb46c1273b6592e503dfe3341c51f15334968
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-01-16 22:11:05 -0300

    first game c#

---
+++ FirstGame/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Write(@"
+██████╗░███████╗░██████╗░█████╗░██╗░░░██╗██████╗░██████╗░░█████╗░  ░█████╗░
+██╔══██╗██╔════╝██╔════╝██╔══██╗██║░░░██║██╔══██╗██╔══██╗██╔══██╗  ██╔══██╗
+██║░░██║█████╗░░╚█████╗░██║░░╚═╝██║░░░██║██████╦╝██████╔╝███████║  ██║░░██║
+██║░░██║██╔══╝░░░╚═══██╗██║░░██╗██║░░░██║██╔══██╗██╔══██╗██╔══██║  ██║░░██║
+██████╔╝███████╗██████╔╝╚█████╔╝╚██████╔╝██████╦╝██║░░██║██║░░██║  ╚█████╔╝
+╚═════╝░╚══════╝╚═════╝░░╚════╝░░╚═════╝░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝  ░╚════╝░
+
+███╗░░██╗██╗░░░██╗███╗░░░███╗███████╗██████╗░░█████╗░
+████╗░██║██║░░░██║████╗░████║██╔════╝██╔══██╗██╔══██╗
+██╔██╗██║██║░░░██║██╔████╔██║█████╗░░██████╔╝██║░░██║
+██║╚████║██║░░░██║██║╚██╔╝██║██╔══╝░░██╔══██╗██║░░██║
+██║░╚███║╚██████╔╝██║░╚═╝░██║███████╗██║░░██║╚█████╔╝
+╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚═╝░╚════╝░");
+
+//Gerar um número aleatório entre 1 e 100
+Random random = new Random();
+int numRandom = random.Next(1, 101);
+
+//Receber palpite do usuário
+Console.WriteLine();
+Console.Write("\nDigite um número: ");
+int numUser = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+//Verificar se o valor digitado pelo usuário é menor ou maior ao escolhido
+do
+{
+    if(numUser < numRandom)
+    {
+        Console.Write("Errado! O número que eu escolhi é maior que " + numUser + "... \nTente novamente: ");
+        numUser = int.Parse(Console.ReadLine()!);
+        Console.WriteLine();
+    
+    }else if(numUser > numRandom)
+    {
+        Console.Write("Errado! O número que eu escolhi é menor que " + numUser + "... \nTente novamente: ");
+        numUser = int.Parse(Console.ReadLine()!);
+        Console.WriteLine();
+    
+    }
+
+} while (numUser != numRandom); //Repetir enquanto o usuário não acertar
+
+Console.WriteLine("Parabéns, você acertou! O número que eu escolhi foi " + numRandom);
